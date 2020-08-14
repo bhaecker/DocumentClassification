a9f8d981189d6c68087377b9cc6ec233c4caaadd
bhaecker <e11713128@student.tuwien.ac.at>
2020-08-14 14:06:31 +0200

    fixed directories

--- Data/Preprocessing.py
+++ Data/Preprocessing.py
@@ -31,26 +31,26 @@ def make_split(split):
         images = np.asarray(images)
         images = np.stack((images[:][:][:], images[:][:][:], images[:][:][:]), axis=3)
         images = images.astype('float16')
-        np.save('Tobacco_train/' + label + '.npy', images)
+        np.save('Data/Tobacco_train/' + label + 's.npy', images)
 
         images = [cv2.resize(imageio.imread(path), (244, 244)) for path in test_array_split]
         images = np.asarray(images)
         images = np.stack((images[:][:][:], images[:][:][:], images[:][:][:]), axis=3)
         images = images.astype('float16')
-        np.save('Tobacco_test/' + label + '.npy', images)
+        np.save('Data/Tobacco_test/' + label + '.npy', images)
 
         images = [cv2.resize(imageio.imread(path), (244, 244)) for path in unseen_array_split]
         images = np.asarray(images)
         images = np.stack((images[:][:][:], images[:][:][:], images[:][:][:]), axis=3)
         images = images.astype('float16')
-        np.save('Tobacco_unseen/' + label + '.npy', images)
+        np.save('Data/Tobacco_unseen/' + label + '.npy', images)
 
         #label the classes
         target = np.zeros(len(labels))
         target[counter] = 1
-        np.save('Tobacco_train/' + label + '_target.npy', target)
-        np.save('Tobacco_test/' + label + '_target.npy', target)
-        np.save('Tobacco_unseen/' + label + '_target.npy', target)
+        np.save('Data/Tobacco_train/' + label + '_target.npy', target)
+        np.save('Data/Tobacco_test/' + label + '_target.npy', target)
+        np.save('Data/Tobacco_unseen/' + label + '_target.npy', target)
 
 split = [60,20,20]
 
