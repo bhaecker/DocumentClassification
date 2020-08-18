14af006349d35e5e3360d92a2021e93fd4e7d38f
bhaecker <e11713128@student.tuwien.ac.at>
2020-08-18 12:23:17 +0200

    plot training

--- SuperviseTraining.py
+++ SuperviseTraining.py
@@ -7,8 +7,8 @@ history=np.load('history_topBlocks.npy',allow_pickle='TRUE').item()
 
 print(history)
 
-plt.plot(history['loss'])
-#plt.plot(history['val_acc'])
+plt.plot(history.history['loss'])
+plt.plot(history['val_acc'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
