d379f4185cbec8a2282d363c12e4d9737ae2550c
bhaecker <e11713128@student.tuwien.ac.at>
2020-09-21 14:41:04 +0200

    new experiment

--- __main__.py
+++ __main__.py
@@ -31,7 +31,7 @@ def __main__():
     #CNN_model = load_model('model_100_epochs.h5')
 
     #
-    list_methods = [margin_sampling_fn,diversity_method,metric_method]
+    list_methods = [RL_CNN_method,RL_human_method,metric_method]
     experiment_single('model_100_epochs.h5',epochs_retrain,retrain_batch,batch_size,list_methods)
 
 if __name__ == "__main__":
